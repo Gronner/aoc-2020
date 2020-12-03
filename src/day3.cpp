db2a75a0b3b138f73cd3cc656136860b0b33103b
Gronner <felix.braeunling@t-online.de>
2020-12-03 18:16:30 +0100

    Factor out computation of hit trees

--- src/day3.cpp
+++ src/day3.cpp
@@ -4,6 +4,13 @@
 
 #include "aoc_input.hpp"
 
+static unsigned int count_trees(
+    const std::vector<std::string>& input_data, 
+    const unsigned int speed_right,
+    const unsigned int speed_down,
+    const unsigned int hill_height,
+    const unsigned int hill_part_width);
+
 unsigned int sleeding(std::vector<std::string> input_data) {
     constexpr int speed_sets[5][2] = {{1, 1}, {3, 1}, {5, 1}, {7, 1}, {1, 2}};
     const auto hill_height = input_data.size() - 1;
@@ -11,21 +18,11 @@ unsigned int sleeding(std::vector<std::string> input_data) {
 
     unsigned int total_tree_count = 0;
     for(auto& speed_set: speed_sets) {
-        unsigned int position_down = 0;
-        unsigned int position_right = 0;
-        unsigned int speed_right = speed_set[0];
-        unsigned int speed_down = speed_set[1];
-
-
-        unsigned int tree_counter = 0;
-        while(position_down <= hill_height) {
-            if(input_data[position_down].at(position_right) == '#') {
-                tree_counter++;
-            }
-            position_down += speed_down;
-            position_right += speed_right;
-            position_right %= hill_part_width;
-        }
+        unsigned int tree_counter = count_trees(input_data, 
+                                                speed_set[0],
+                                                speed_set[1],
+                                                hill_height,
+                                                hill_part_width);
 
         if(total_tree_count == 0) {
             total_tree_count += tree_counter;
@@ -35,4 +32,24 @@ unsigned int sleeding(std::vector<std::string> input_data) {
     }
 
     return total_tree_count;
+}
+
+static unsigned int count_trees(
+    const std::vector<std::string>& input_data, 
+    const unsigned int speed_right,
+    const unsigned int speed_down,
+    const unsigned int hill_height,
+    const unsigned int hill_part_width) {
+    unsigned int position_down = 0;
+    unsigned int position_right = 0;
+    unsigned int tree_counter = 0;
+    while(position_down <= hill_height) {
+        if(input_data[position_down].at(position_right) == '#') {
+            tree_counter++;
+        }
+        position_down += speed_down;
+        position_right += speed_right;
+        position_right %= hill_part_width;
+    }
+    return tree_counter;
 }
