ea5fee33aa969c2f5c4f746f6e15d0b85b58bb43
Gronner <felix.braeunling@t-online.de>
2020-12-03 15:54:57 +0100

    Add test for Day 1 solution

--- test/day1_tests.cpp
+++ test/day1_tests.cpp
@@ -1,5 +1,15 @@
 #include "gtest/gtest.h"
 
-TEST(Day1Test, FailingTest) {
-    ASSERT_TRUE(false);
+#include "aoc_input.hpp"
+#include "data.hpp"
+#include "days.hpp"
+
+TEST(Day1Test, SampleInput) {
+    std::vector<unsigned int> input_data = {1721, 979, 366, 299, 675, 1456};
+    ASSERT_EQ(241861950, expense_report(input_data));
+}
+
+TEST(Day1Test, RiddleInput) {
+    auto input_data = aoc::read_input_tokenwise<unsigned int>(day1_data);
+    ASSERT_EQ(70276940, expense_report(input_data));
 }
