98c148eb6ef5694516345b7c8538fd3066c058d9
Gronner <felix.braeunling@t-online.de>
2020-12-05 10:30:52 +0100

    Limit usage of pow_2, to increase readability

--- src/day5.cpp
+++ src/day5.cpp
@@ -17,7 +17,7 @@ static unsigned int pow_2(const unsigned int value) {
 
 static unsigned int follow_binary_path(const std::string path, const char upper_letter, const unsigned int path_len) {
     unsigned int current_value = 0;
-    unsigned int current_step = path_len;
+    unsigned int current_step = pow_2(path_len);
 
     for(auto letter: path) {
         current_step /= 2;
@@ -32,8 +32,8 @@ static unsigned int identify_seat_id(const std::string boarding_pass, const Plan
     const auto row_description = boarding_pass.substr(0, plane.row_path_len);
     const auto column_description = boarding_pass.substr(7, plane.column_path_len);
 
-    const auto row = follow_binary_path(row_description, 'B', pow_2(plane.row_path_len));
-    const auto column = follow_binary_path(column_description, 'R', pow_2(plane.column_path_len));
+    const auto row = follow_binary_path(row_description, 'B', plane.row_path_len);
+    const auto column = follow_binary_path(column_description, 'R', plane.column_path_len);
     return column + 8 * row;
 }
 
