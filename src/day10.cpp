f2cee9e7481097efac411293015bd6454fe9233d
Gronner <felix.braeunling@t-online.de>
2020-12-10 09:09:05 +0100

    Add day 10 part 2 solution

--- src/day10.cpp
+++ src/day10.cpp
@@ -8,7 +8,7 @@ static unsigned int to_number(std::string& input) {
     return std::stoul(input);
 }
 
-unsigned int adapter_chaining(std::vector<std::string> input_data) {
+unsigned long long adapter_chaining(std::vector<std::string> input_data) {
     std::vector<unsigned int> adapters;
     std::transform(input_data.begin(), input_data.end(), std::back_inserter(adapters), to_number);
     std::sort(adapters.begin(), adapters.end());
@@ -16,19 +16,36 @@ unsigned int adapter_chaining(std::vector<std::string> input_data) {
     adapters.push_back(device_rating);
     adapters.insert(adapters.begin(), 0);
 
-    unsigned int one_jolt_diff_count = 0;
-    unsigned int three_jolt_diff_count = 0;
-    for(auto adapter = adapters.begin() + 1; adapter != adapters.end(); ++adapter) {
-        std::cout << *adapter << std::endl;
-       if(1 == *adapter - *(adapter - 1)) {
-           one_jolt_diff_count++;
-           continue;
-       }
-       if(3 == *adapter - *(adapter - 1)) {
-           three_jolt_diff_count++;
-           continue;
-       }
+    std::vector<unsigned int> differentials;
+
+    for(auto adapter = adapters.begin() + 1; adapter != adapters.end(); adapter++) {
+        differentials.push_back(*adapter - *(adapter - 1));
     }
 
-    return one_jolt_diff_count * three_jolt_diff_count;
+    std::vector<unsigned int> gap_sizes;
+    unsigned int jumped = 0;
+    for(auto differential: differentials) {
+        if(differential == 1) {
+            jumped++;
+        } else if(differential == 3) {
+            gap_sizes.push_back(jumped);
+            jumped = 0;
+        }
+    }
+    gap_sizes.push_back(jumped);
+
+    unsigned long long product = 1;
+    for(auto gap_size: gap_sizes) {
+        switch(gap_size) {
+            case 2: product = product * 2;
+                      break;
+            case 3: product = product * 4;
+                      break;
+            case 4: product = product * 7;
+                      break;
+            default: break;
+        }
+    }
+
+    return product;
 }
