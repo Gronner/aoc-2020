8ed64c620880cabb82133a7e51e059f75b27c64d
Gronner <felix.braeunling@t-online.de>
2020-12-11 08:56:29 +0100

    Add day 11 part 2 solution

--- src/day11.cpp
+++ src/day11.cpp
@@ -1,24 +1,8 @@
 #include "days.hpp"
 
 #include <algorithm>
-#include <iostream>
 
 unsigned int musical_chairs(std::vector<std::string> seat_layout) {
-    /*
-    seat_layout  = {
-        "L.LL.LL.LL",
-        "LLLLLLL.LL",
-        "L.L.L..L..",
-        "LLLL.LL.LL",
-        "L.LL.LL.LL",
-        "L.LLLLL.LL",
-        "..L.L.....",
-        "LLLLLLLLLL",
-        "L.LLLLLL.L",
-        "L.LLLLL.LL"
-    };
-    */
-
     // First Round all seats get taken
     std::string extra_row(seat_layout[0].size(), '*');
     seat_layout.insert(seat_layout.begin(), extra_row);
@@ -35,19 +19,38 @@ unsigned int musical_chairs(std::vector<std::string> seat_layout) {
         auto seat_layout_tmp = seat_layout;
         for(auto row = 1U; row != seat_layout.size() - 1; ++row) {
             for(auto place = 1U; place != seat_layout[row].size() - 1; ++place) {
+
                 auto adjacent_taken_seats = 0U;
+
                 if(seat_layout[row].at(place) == '.') {
                     continue;
                 }
-                adjacent_taken_seats += std::count_if((*(seat_layout.begin() + row - 1)).begin() + place - 1,
-                        (*(seat_layout.begin() + row - 1)).begin() + place + 2,
-                        [](char c){ return '#' == c;});
-                adjacent_taken_seats += (seat_layout[row].at(place + 1) == '#') ? 1 : 0;
-                adjacent_taken_seats += (seat_layout[row].at(place - 1) == '#') ? 1 : 0;
-                adjacent_taken_seats += std::count_if((*(seat_layout.begin() + row + 1)).begin() + place - 1,
-                        (*(seat_layout.begin() + row + 1)).begin() + place + 2,
-                        [](char c){ return '#'  == c;});
-                if(4 <= adjacent_taken_seats) {
+
+                const std::vector<std::pair<int, int>> vectors = { 
+                    std::make_pair(1,0),
+                    std::make_pair(1,1),
+                    std::make_pair(0,1),
+                    std::make_pair(-1,1),
+                    std::make_pair(-1,0),
+                    std::make_pair(-1,-1),
+                    std::make_pair(0,-1),
+                    std::make_pair(1,-1)
+                };
+                
+                for(auto direction: vectors) {
+                    int distance_x = direction.first;
+                    int distance_y = direction.second;
+                    while(seat_layout[row + distance_y].at(place + distance_x) == '.') {
+                        distance_x += direction.first;
+                        distance_y += direction.second;
+                    }
+
+                    if(seat_layout[row + distance_y].at(place + distance_x) == '#') {
+                        adjacent_taken_seats++;
+                    }
+                }
+
+                if(5 <= adjacent_taken_seats) {
                     if('#' == seat_layout[row].at(place)) {
                         seat_layout_tmp[row][place] = 'L';
                         seats_changed++;
