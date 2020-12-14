1e55f516956bb22938660072beb9fb74c34a5060
Gronner <felix.braeunling@t-online.de>
2020-12-14 08:08:09 +0100

    Add day 14 part 2 solution

--- src/day14.cpp
+++ src/day14.cpp
@@ -5,9 +5,39 @@
 
 #include "parsing.hpp"
 
+std::vector<std::string> generate_masks(std::string input_mask) {
+    std::vector<size_t> x_indicies;
+    size_t enumerate = 0;
+    for(auto bit: input_mask) {
+        if('X' == bit) {
+            x_indicies.push_back(enumerate);
+        }
+        enumerate++;
+    }
+    const auto combinations = 1ULL << x_indicies.size();
+    std::vector<std::string> masks(combinations, input_mask);
+    auto stepsize = 2ULL;
+    auto x_idx = x_indicies.begin();
+    do {
+        auto count = 1ULL;
+        for(auto& mask: masks) {
+            if(count < (stepsize / 2)) {
+                mask[*x_idx] = '0';
+            } else { 
+                mask[*x_idx] = '1';
+            }
+            count = (count + 1) % stepsize;
+        }
+        x_idx++;
+        stepsize <<= 1;
+    } while(x_idx != x_indicies.end());
+    return masks;
+}
+
 uint64_t dock_decoding(std::vector<std::string> input_data) {
     std::map<uint64_t, uint64_t> memory;
-    std::string mask = "";
+    std::vector<std::string> masks;
+    std::string mask;
     for(const auto line: input_data) {
         auto entry = aoc::split_on_delimiter(line, " = ");
         if("mask" == entry[0]) {
@@ -15,23 +45,22 @@ uint64_t dock_decoding(std::vector<std::string> input_data) {
             continue;
         } 
         auto address = aoc::grab_information(entry[0], "mem\\[(\\d*)\\]");
-        uint64_t address_value = std::stoll(address[1]);
+        std::string address_value = std::bitset<36>(std::stoll(address[1])).to_string();
         uint64_t mem_value = std::stoll(entry[1]);
 
         uint64_t bit_counter = 0;
-        for(auto bit = mask.rbegin(); bit != mask.rend(); ++bit) {
-            if(*bit == 'X') {
-                bit_counter ++;
-                continue;
-            }
-            if(*bit == '0') {
-                mem_value &= ~(1ULL << bit_counter);
-            } else {
-                mem_value |= 1ULL << bit_counter;
+        for(auto bit = mask.begin(); bit != mask.end(); ++bit) {
+            if(*bit == '1') {
+                address_value[bit_counter] = '1';
+            } else if(*bit == 'X') {
+                address_value[bit_counter] = 'X';
             }
             bit_counter++;
         }
-        memory[address_value] = mem_value;
+        auto memory_addresses = generate_masks(address_value);
+        for(auto memory_address: memory_addresses) {
+            memory[std::stoll(memory_address, 0, 2)] = mem_value;
+        }
     }
     uint64_t sum = 0;
     for(auto value: memory) {
