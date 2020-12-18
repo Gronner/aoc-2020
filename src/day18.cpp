57e1396bb407e9f6022270f4c883b26b64d5d809
Gronner <felix.braeunling@t-online.de>
2020-12-18 09:14:06 +0100

    Add day 18 part 2 solution

--- src/day18.cpp
+++ src/day18.cpp
@@ -1,53 +1,72 @@
 #include "days.hpp"
 
-#include <iostream>
-
-uint64_t recursive_math(std::string::iterator& begin, const std::string::iterator end) {
-    uint64_t value = 0;
-    char next_operation = 'X';
-    while(begin != end) {
-        if('(' == *begin) {
-            begin++;
-            if('+' == next_operation) {
-                value += recursive_math(begin, end);
-            } else if('*' == next_operation) {
-                value *= recursive_math(begin, end);
-            } else {
-                value = recursive_math(begin, end);
+std::vector<char> shunting_yard(std::string line) {
+    static std::vector<char> operators{'*' , '+'};
+    std::vector<char> output_queue;
+    std::vector<char> operator_queue;
+    for(auto token: line) {
+        if(token >= '0' && token <= '9') {
+            output_queue.push_back(token);
+        } else if(operators.end() != std::find(operators.begin(), operators.end(), token)) {
+            while((0 != operator_queue.size()) && 
+                (std::distance(operators.begin(), std::find(operators.begin(), operators.end(), token)) <= std::distance(operators.begin(), std::find(operators.begin(), operators.end(), operator_queue.back())) && '(' != operator_queue.back())) {
+                output_queue.push_back(operator_queue.back());
+                operator_queue.pop_back();
             }
-        } else if(' ' == *begin) {
-            begin++;
-        } else if('+' == *begin) {
-            begin++;
-            next_operation = '+';
-        } else if('*' == *begin) {
-            begin++;
-            next_operation = '*';
-        } else if(')' == *begin) {
-            begin++;
-            return value;
-        } else {
-            uint64_t current_digit = (*begin) - '0';
-            begin++;
-            if('+' == next_operation) {
-                value += current_digit;
-            } else if('*' == next_operation) {
-                value *= current_digit;
-            } else {
-                value = current_digit;
+            operator_queue.push_back(token);
+        } else if('(' == token) {
+            operator_queue.push_back(token);
+        } else if(')' == token) {
+            while('(' != operator_queue.back()) {
+                auto tmp = operator_queue.back();
+                output_queue.push_back(tmp);
+                operator_queue.pop_back();
+            }
+            if('(' == operator_queue.back()) {
+                operator_queue.pop_back();
             }
         }
-
     }
-    return value;
+    while(0 != operator_queue.size()) {
+        output_queue.push_back(operator_queue.back());
+        operator_queue.pop_back();
+    }
+    return output_queue;
+}
+
+uint64_t compute_postfix(std::vector<char> postfix_notation) {
+    std::vector<uint64_t> stack;
+    for(auto c: postfix_notation) {
+        if('0' <= c && c <= '9') {
+            stack.push_back(c - '0');
+        } else {
+            auto val1 = stack.back();
+            stack.pop_back();
+            auto val2 = stack.back();
+            stack.pop_back();
+            switch(c) {
+                case '+': stack.push_back(val1 + val2);
+                          break;
+                case '*': stack.push_back(val1 * val2);
+                          break;
+                default: break;
+            }
+        }
+    }
+    return stack.back();
 }
 
 uint64_t homework(std::vector<std::string> input_data) {
+    /*
+    input_data = {
+        "1 + 2 * 3 + 4 * 5 + 6",
+        "1 + (2 * 3) + (4 * (5 + 6))",
+    };
+    */
     uint64_t sum = 0;
     for(auto line: input_data) {
-        auto start = line.begin();
-        auto value = recursive_math(start, line.end());
-        sum += value;
+        auto postfix_notation = shunting_yard(line);
+        sum += compute_postfix(postfix_notation);
     }
     return sum;
 }
