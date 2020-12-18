01e0a11ca0d3d143b586085239c39907a10a3e0d
Gronner <felix.braeunling@t-online.de>
2020-12-18 23:24:30 +0100

    Remove commented sample input

--- src/day16/day16.cpp
+++ src/day16/day16.cpp
@@ -7,21 +7,6 @@
 #include "parsing.hpp"
 
 uint64_t ticket_validation(std::vector<std::string> input_data) {
-    /*
-    input_data = {
-        "class: 0-1 or 4-19",
-        "row: 0-5 or 8-19",
-        "seat: 0-13 or 16-19",
-        "",
-        "your ticket:",
-        "11,12,13",
-        "",
-        "nearby tickets:",
-        "3,9,18",
-        "15,1,5",
-        "5,14,9",
-    };
-    */
     auto mode = 0;
     std::vector<Rule> rules;
     std::vector<std::vector<uint64_t>> tickets;
