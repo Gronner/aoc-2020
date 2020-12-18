73a5dd1672adc96584ecd55a3dc57d60af4850fb
Gronner <felix.braeunling@t-online.de>
2020-12-19 00:02:16 +0100

    Refactor day 18 to increase readability and const correctnes

--- src/day18.cpp
+++ src/day18.cpp
@@ -1,50 +1,70 @@
 #include "days.hpp"
 
-std::vector<char> shunting_yard(std::string line) {
-    static std::vector<char> operators{'*' , '+'};
+template<typename T>
+static auto get_index(const std::vector<T> vec, const T value) {
+    return std::distance(vec.cbegin(), std::find(vec.cbegin(), vec.cend(), value));
+}
+
+template<typename T>
+static auto pop(std::vector<T>& vec) {
+    const auto tmp = vec.back();
+    vec.pop_back();
+    return tmp;
+}
+
+static const std::vector<char> operators{'*' , '+'};
+static bool is_higher_precedence(const char token1, const char token2 ) {
+    return get_index(operators, token1) <= get_index(operators, token2);
+}
+
+static bool is_operator(const char token) {
+    return operators.end() != std::find(operators.begin(), operators.end(), token);
+}
+
+static bool is_digid(const char token) {
+    return '0' <= token && token <= '9';
+}
+
+static std::vector<char> shunting_yard(const std::string line) {
+    static const std::vector<char> operators{'*' , '+'};
     std::vector<char> output_queue;
     std::vector<char> operator_queue;
-    for(auto token: line) {
-        if(token >= '0' && token <= '9') {
+    for(const auto token: line) {
+        if(is_digid(token)) {
             output_queue.push_back(token);
-        } else if(operators.end() != std::find(operators.begin(), operators.end(), token)) {
-            while((0 != operator_queue.size()) && 
-                (std::distance(operators.begin(), std::find(operators.begin(), operators.end(), token)) <= std::distance(operators.begin(), std::find(operators.begin(), operators.end(), operator_queue.back())) && '(' != operator_queue.back())) {
-                output_queue.push_back(operator_queue.back());
-                operator_queue.pop_back();
+        } else if(is_operator(token)) {
+            while(!operator_queue.empty() && is_higher_precedence(token, operator_queue.back()) && '(' != operator_queue.back()) {
+                output_queue.push_back(pop(operator_queue));
             }
             operator_queue.push_back(token);
         } else if('(' == token) {
             operator_queue.push_back(token);
         } else if(')' == token) {
             while('(' != operator_queue.back()) {
-                auto tmp = operator_queue.back();
-                output_queue.push_back(tmp);
-                operator_queue.pop_back();
+                output_queue.push_back(pop(operator_queue));
             }
             if('(' == operator_queue.back()) {
                 operator_queue.pop_back();
             }
         }
     }
-    while(0 != operator_queue.size()) {
-        output_queue.push_back(operator_queue.back());
-        operator_queue.pop_back();
+    while(!operator_queue.empty()) {
+        output_queue.push_back(pop(operator_queue));
     }
     return output_queue;
 }
 
-uint64_t compute_postfix(std::vector<char> postfix_notation) {
+static uint64_t compute_postfix(const std::vector<char> postfix_notation) {
     std::vector<uint64_t> stack;
-    for(auto c: postfix_notation) {
-        if('0' <= c && c <= '9') {
-            stack.push_back(c - '0');
+    for(const auto token: postfix_notation) {
+        if('0' <= token && token <= '9') {
+            stack.push_back(token - '0');
         } else {
-            auto val1 = stack.back();
+            const auto val1 = stack.back();
             stack.pop_back();
-            auto val2 = stack.back();
+            const auto val2 = stack.back();
             stack.pop_back();
-            switch(c) {
+            switch(token) {
                 case '+': stack.push_back(val1 + val2);
                           break;
                 case '*': stack.push_back(val1 * val2);
@@ -56,16 +76,10 @@ uint64_t compute_postfix(std::vector<char> postfix_notation) {
     return stack.back();
 }
 
-uint64_t homework(std::vector<std::string> input_data) {
-    /*
-    input_data = {
-        "1 + 2 * 3 + 4 * 5 + 6",
-        "1 + (2 * 3) + (4 * (5 + 6))",
-    };
-    */
+uint64_t homework(const std::vector<std::string> input_data) {
     uint64_t sum = 0;
-    for(auto line: input_data) {
-        auto postfix_notation = shunting_yard(line);
+    for(const auto line: input_data) {
+        const auto postfix_notation = shunting_yard(line);
         sum += compute_postfix(postfix_notation);
     }
     return sum;
