ba12000bf9a0ab7cb826105f86dfa8ef82ca56b0
Felix Bräunling <felix.braeunling@methodpark.de>
2020-12-21 14:23:43 +0100

    Move to relative paths

--- src/data.hpp
+++ src/data.hpp
@@ -2,8 +2,8 @@
 
 #include <string>
 
-const std::string data_dir = "/home/felix/DataDisk/Projekte/aoc-2020/data/";
-const std::string  day1_data = data_dir + "day1.txt";
+const std::string data_dir = "../data/";
+const std::string day1_data = data_dir + "day1.txt";
 const std::string day2_data = data_dir + "day2.txt";
 const std::string day3_data = data_dir + "day3.txt";
 const std::string day4_data = data_dir + "day4.txt";
